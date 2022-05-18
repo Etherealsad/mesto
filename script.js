f5d3098f9b58c6b9a7e102f18e1b6d9ca9850b4d
Mihail Kuksenok <etherealsad@gmail.com>
2022-05-18 23:07:58 +0700

    3

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 let popup = document.querySelector(".popup");
 let closePopup = document.querySelector(".popup__close-button");
-let formElement = document.querySelector(".popup__container");
-let nameInput = document.querySelector(".popup__input-title");
-let jobInput = document.querySelector(".popup__input-subtitle");
+let formElement = document.querySelector(".popup__form");
+let nameInput = document.querySelector(".popup__input_title");
+let jobInput = document.querySelector(".popup__input_subtitle");
 let profileName = document.querySelector(".profile__title");
 let profileJob = document.querySelector(".profile__subtitle");
 let profileEdit = document.querySelector(".profile__edit");
-let like = document.querySelectorAll(".element__like");
+/* let like = document.querySelectorAll(".element__like"); */
 
 function poupOpen() {
-    popup.classList.toggle("popup__open");
+    popup.classList.toggle("popup_open");
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 }
 
+function popupClose() {
+    popup.classList.remove("popup_open");
+
+}
+
 profileEdit.addEventListener("click", poupOpen);
-closePopup.addEventListener("click", poupOpen);
+closePopup.addEventListener("click", popupClose);
 
 function formSubmitHandler(evt) {
     evt.preventDefault();
@@ -26,8 +31,8 @@ function formSubmitHandler(evt) {
 
 formElement.addEventListener("submit", formSubmitHandler);
 
-for (let i= 0; i < like.length; i++ ) {
+/* for (let i= 0; i < like.length; i++ ) {
     like[i].addEventListener ("click", function() {
         like[i].classList.toggle("element__like-active");
     });
-}
+} */
